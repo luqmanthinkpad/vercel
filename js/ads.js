0d545a8723c4f83ec1283a7dbcfd19aaf153c610
luqmanthinkpad <luqmanthinkpad@gmail.com>
2026-02-05 17:40:29 +0700

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -7,7 +7,7 @@ const showMyAds = () => {
             popup.style.display = 'flex';
 
             window.atOptions = {
-                'key' : 'c6519a79b77606d968cf36c00f3894c6',
+                'key' : 'a215683d2d0ce8fecd54e01b99606d75',
                 'format' : 'iframe',
                 'height' : 250,
                 'width' : 300,
@@ -16,7 +16,7 @@ const showMyAds = () => {
 
             const adScript = document.createElement('script');
             adScript.type = 'text/javascript';
-            adScript.src = 'https://www.highperformanceformat.com/c6519a79b77606d968cf36c00f3894c6/invoke.js';
+            adScript.src = 'https://anguishgrandpa.com/a215683d2d0ce8fecd54e01b99606d75/invoke.js';
             
             adsPlaceholder.innerHTML = ''; 
             adsPlaceholder.appendChild(adScript);
@@ -31,14 +31,14 @@ const fillHomeAds = () => {
     const container = document.getElementById('ads-728x90');
     if (container) {
         window.atOptions = {
-            'key' : '6a0b7fb67f4a78d4b0f45804516b9bae',
+            'key' : '6bc878b50f4ca4fe0f9f00a24603655f',
             'format' : 'iframe',
             'height' : 90,
             'width' : 728,
             'params' : {}
         };
         const s = document.createElement('script');
-        s.src = 'https://www.highperformanceformat.com/6a0b7fb67f4a78d4b0f45804516b9bae/invoke.js';
+        s.src = 'https://anguishgrandpa.com/6bc878b50f4ca4fe0f9f00a24603655f/invoke.js';
         container.innerHTML = '';
         container.appendChild(s);
     }
@@ -49,15 +49,16 @@ const fillDetailAds = () => {
     const container = document.getElementById('ads-320x50');
     if (container) {
         window.atOptions = {
-            'key' : '374a452332eafadea63e8466580e4d11',
+            'key' : '659b04a20a0861b7619a7103d607c7d3',
             'format' : 'iframe',
             'height' : 20,
             'width' : 320,
             'params' : {}
         };
         const s = document.createElement('script');
-        s.src = 'https://www.highperformanceformat.com/374a452332eafadea63e8466580e4d11/invoke.js';
+        s.src = 'https://anguishgrandpa.com/659b04a20a0861b7619a7103d607c7d3/invoke.js';
         container.innerHTML = '';
         container.appendChild(s);
     }
+
 };
